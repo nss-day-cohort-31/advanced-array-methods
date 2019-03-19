d05714f29a5cb7877cd0b4dd53318d048fc6939e
Jisie DM <jisie.david@gmail.com>
2019-03-19 15:40:51 -0500

    lightning exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,12 +8,18 @@ outEl.innerHTML += "<h1>Purchasing Agents</h1>";
     from each business and store it in a new array
 */
 const agents = businesses.map(business => {
-    return business.purchasingAgent
+    return {
+      fullName: `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}`,
+      company: business.companyName,
+      phoneNumber:business.phoneWork
+    }
 })
 
 console.table(agents)
 
 agents.forEach(agent => {
-  outEl.innerHTML += `<h2>${agent.nameFirst} ${agent.nameLast}</h2>`;
+  outEl.innerHTML += `<h2>${agent.fullName}</h2>`;
+  outEl.innerHTML += `<p>${agent.company}</p>`;
+  outEl.innerHTML += `<p>${agent.phoneNumber}</p>`;
   outEl.innerHTML += "<hr/>";
 });
